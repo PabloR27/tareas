dc392ca0f732d52b45b31eb595419be7e7ed8cfc
pablor27 <pabloromeroisac27@gmail.com>
2022-01-26 10:54:53 +0100

    miapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  todo = '';
+  tareas: any[] = [];
+  addTodo() {
+    if (this.todo != '') {
+      this.tareas = [...this.tareas, this.todo]//crea un nuevo array con loque tuviera this.tareas+this.todo
+    }
+    this.todo = '';
+  }
+  borrarTarea(index:any) {
+    this.tareas.splice(index,1);
+    //recorre todo el array y deja solo aquellos diferentes a tareaCompletada
+  }
 }
+
+
